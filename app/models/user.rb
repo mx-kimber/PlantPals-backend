1cc4df09e0f842a6969589dba9abf5446641a64f
Kimber Ogden <mx.kimber@gmail.com>
2023-06-19 13:58:03 -0500

    User validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,9 @@
 class User < ApplicationRecord
   has_secure_password
+ 
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true, uniqueness: true
+  validates :password, presence: true, length: { minimum: 6 }
+  
 end
