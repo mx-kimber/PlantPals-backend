dd1bb1e3b0086709da53ddf3e8cd5efdc76bb932
Kimber Ogden <mx.kimber@gmail.com>
2023-06-19 20:45:31 -0500

    Schedules Update and Destroy actions

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -22,12 +22,33 @@ class SchedulesController < ApplicationController
     end
   end
 
+  def update
+    @schedule = Schedule.find_by(id: params[:id])
+
+    if @schedule
+      if @schedule.update(schedule_params)
+        render json: @schedule
+      else
+        render json: { errors: @schedule.errors.full_messages }, status: :unprocessable_entity
+      end
+    else
+      render json: { errors: ['Schedule not found'] }, status: :not_found
+    end
+  end
+
+  def destroy
+    @schedule = Schedule.find_by(id: params[:id])
+
+    if @schedule.destroy
+      render json: { message: "Schedule destroyed successfully" }
+    else
+      render json: { message: "Deletion canceled" }
+    end
+  end
+
   private
 
   def schedule_params
     params.permit(:user_id, :collected_plant_id, :watering_start_date, :days_to_water)
   end
 end
-
-
-
