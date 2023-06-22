1baf5e6ed3a6c17a8c4f852219b3a8bc3bba5d71
Kimber Ogden <mx.kimber@gmail.com>
2023-06-21 21:54:07 -0500

    Implement show and create actions with plant data retrieval and parameter handling

--- app/controllers/collected_plants_controller.rb
+++ app/controllers/collected_plants_controller.rb
@@ -1,3 +1,4 @@
+require 'http'
 class CollectedPlantsController < ApplicationController
   before_action :authenticate_user
 
@@ -9,16 +10,47 @@ class CollectedPlantsController < ApplicationController
 
   def show
     @collected_plant = CollectedPlant.find(params[:id])
-    render :show
+
+    response = api_get_request("https://house-plants2.p.rapidapi.com/id/#{@collected_plant[:plant_id]}")
+
+    if response.code == 200
+      plant_data = JSON.parse(response.body)
+      @plant_data = {
+        'id' => plant_data['id'],
+        'common_name' => plant_data['Common name (fr.)'] || 'Unknown',
+        'latin_name' => plant_data['Latin name'] || 'Unknown',
+        'img' => plant_data['Img'] || 'Unknown',
+        'watering' => plant_data['Watering'] || 'Unknown',
+        'light_ideal' => plant_data['Light ideal'] || 'Unknown',
+        'light_tolerated' => plant_data['Light tolered'] || 'Unknown',
+        'climate' => plant_data['Climat'] || 'Unknown',
+        'category' => plant_data['Categories'] || 'Unknown',
+        'url' => plant_data['Url'] || 'Resource not found'
+      }
+
+      render json: { collected_plant: @collected_plant, plant_data: @plant_data }
+    else
+      render json: { error: "Failed to retrieve plant data. Error code: #{response.code}" }, status: :unprocessable_entity
+    end
   end
 
   def create
-    @collected_plant = current_user.collected_plants.build(collected_plant_params)
+    plant_id = params[:plant_id]
+    user_id = current_user.id
+    @plant_data = retrieve_plant_data(plant_id)
+
+    @collected_plant = CollectedPlant.new(
+      plant_id: plant_id,
+      user_id: user_id,
+      nickname: params[:nickname],
+      notes: params[:notes]
+    )
 
     if @collected_plant.save
-      render :show
+      redirect_to @collected_plant, notice: "Plant saved to collection successfully."
     else
-      render json: { errors: @collected_plant.errors.full_messages }, status: :unprocessable_entity
+      flash[:error] = @collected_plant.errors.full_messages.join(", ")
+      redirect_to plant_path(plant_id)
     end
   end
 
@@ -49,6 +81,12 @@ class CollectedPlantsController < ApplicationController
   private
 
   def collected_plant_params
-    params.permit(:id, :plant_id, :user_id, :nickname, :custom_image, :notes)
+    params.permit(:plant_id, :user_id, :nickname, :custom_image, :notes)
+  end
+  
+  def retrieve_plant_data(plant_id)
+    url = "https://house-plants2.p.rapidapi.com/id/#{plant_id}"
+    response = api_get_request(url)
+    JSON.parse(response.body)
   end
 end
